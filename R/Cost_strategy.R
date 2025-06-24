5ff0039407d3152e82918345c8178227d30b2f20
Roshwini Muthiah <roshwini.muthiah24@imperial.ac.uk>
2025-06-24 12:26:19 +0100

    Hospital burden removed

--- R/Cost_strategy.R
+++ R/Cost_strategy.R
@@ -7,7 +7,3 @@ calculate_vaccine_cost <- function(population, uptake, cost_vaccine, cost_delive
   return(sum((cost_vaccine + cost_delivery) * population * uptake, na.rm = TRUE))
 }
 
-# Hospital Burden = Hospitalisations × Avg Length of Stay × Cost per Day
-calculate_hospital_burden <- function(hospitalisations, los, cost_per_day) {
-  return(sum(hospitalisations * los * cost_per_day, na.rm = TRUE))
-}
